c02b93466db161c696ece68584774ce52ce39cca
gabao55 <gabriel.s.rosin@gmail.com>
2022-06-08 20:38:49 -0300

    Refactors function

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,23 +1,42 @@
 function selectItem(item) {
+    let checkedItem = checkItemSelection(item);
+    if (checkedItem) {
+        return true
+    }
+    
+    cleanCarouselSelection(item.parentElement.querySelectorAll(".option"));
+    
+    setIconToOption(item);
+
+    return true
+}
+
+function checkItemSelection(item) {
     if (item.classList.contains("selected")) {
         item.classList.remove("selected");
-        return item.removeChild(item.querySelector("ion-icon"));
+        item.removeChild(item.querySelector("ion-icon"));
+        return true
     }
 
-    let carousel = item.parentElement.querySelectorAll(".option");
+    return false
+}
+
+function cleanCarouselSelection(carousel) {
     let child;
 
     for (let i=0; i < carousel.length; i++) {
         child = carousel[i];
-
         if (child.classList.contains("selected")) {
             child.classList.remove("selected");
             child.removeChild(child.querySelector("ion-icon"));
         }
     }
+}
+
+function setIconToOption(item) {
     let icon = document.createElement(
         "ion-icon"
-    )
+    );
     icon.setAttribute("name", "checkmark-circle");
     item.classList.add("selected");
     item.appendChild(icon);
