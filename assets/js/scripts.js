29a27a75960cc76dbd9c77b323be48d57eb59e24
gabao55 <gabriel.s.rosin@gmail.com>
2022-06-09 18:11:22 -0300

    Implements features for displaying the order elements, prices, total prices and building the message that is going to be sent to whatsapp

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,3 +1,13 @@
+let finishButton = document.querySelector(".footer button");
+let notFinishedButton = document.querySelector(".footer button p").cloneNode(true);
+let finalPage = document.querySelector(".final-page");
+let itemsFinalPage = document.querySelectorAll(".final-page span");
+let totalPrice = document.querySelectorAll(".final-page p")[1];
+let totalPriceSum;
+let selectedMainCourse;
+let selectedDrink;
+let selectedDessert;
+
 function selectItem(item) {
     let checkedItem = checkItemSelection(item);
     if (checkedItem) {
@@ -42,10 +52,6 @@ function setIconToOption(item) {
     item.appendChild(icon);
 }
 
-let finishButton = document.querySelector(".footer button");
-let notFinishedButton = document.querySelector(".footer button p").cloneNode(true);
-let finalPage = document.querySelector(".final-page");
-
 document.addEventListener("click", () => {
     if (document.querySelectorAll(".selected").length == 3) {
         finishButton.style.backgroundColor = "#32B72F";
@@ -58,11 +64,43 @@ document.addEventListener("click", () => {
 })
 
 function showFinalPage() {
-    let name = prompt("Por favor, informe seu nome");
-    let address = prompt("Por favor, informe seu endereço");
     if (document.querySelectorAll(".selected").length == 3) {
+        itemsFinalPage[0].innerText = document.querySelector(".main-course .selected h3").innerText;
+        itemsFinalPage[2].innerText = document.querySelector(".drink .selected h3").innerText;
+        itemsFinalPage[4].innerText = document.querySelector(".dessert .selected h3").innerText;
+        itemsFinalPage[1].innerText = document.querySelector(".main-course .selected span").innerText.slice(3);
+        itemsFinalPage[3].innerText = document.querySelector(".drink .selected span").innerText.slice(3);
+        itemsFinalPage[5].innerText = document.querySelector(".dessert .selected span").innerText.slice(3);
+
+        totalPriceSum = Number(itemsFinalPage[1].innerText.replace(",", ".")) + Number(itemsFinalPage[3].innerText.replace(",", ".")) + Number(itemsFinalPage[5].innerText.replace(",", "."));
+        totalPriceSum = totalPriceSum.toFixed(2);
+        totalPriceSum = String(totalPriceSum).replace(".", ",");
+
+        totalPrice.innerText = "R$ " + totalPriceSum;
+        totalPrice.innerText;
         finalPage.classList.toggle("display-none");
     }
+}
+
+function sendToWhatsapp() {
+    let name = prompt("Por favor, informe seu nome");
+    let address = prompt("Por favor, informe seu endereço");
+    selectedMainCourse = document.querySelector(".main-course .selected h3").innerText;
+    selectedDrink = document.querySelector(".drink .selected h3").innerHTML;
+    selectedDessert = document.querySelector(".dessert .selected h3").innerText;
+    
+
+    let message = `Olá, gostaria de fazer o pedido:\n
+    - Prato: ${selectedMainCourse}\n
+    - Bebida: ${selectedDrink}\n
+    - Sobremesa: ${selectedDessert}\n
+    Total: R$ ${totalPriceSum}\n
+    \n
+    Nome: ${name}\n
+    Endereço: ${address}`;
+
+    console.log(message);
+
 }
 
 function hideFinalPage() {
