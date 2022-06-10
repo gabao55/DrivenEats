58b421b1f6712ef80b2cb0a90b2d589d03b673cf
gabao55 <gabriel.s.rosin@gmail.com>
2022-06-10 16:22:39 -0300

    Organizes file

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -49,8 +49,11 @@ function setIconToOption(item) {
         "ion-icon"
     );
     icon.setAttribute("name", "checkmark-circle");
+
     item.classList.add("selected");
     item.appendChild(icon);
+
+    return true;
 }
 
 document.addEventListener("click", () => {
@@ -62,12 +65,15 @@ document.addEventListener("click", () => {
         finishButton.style.backgroundColor = "#CBCBCB";
         finishButton.replaceChildren(notFinishedButton);
     }
+
+    return true;
 })
 
 function showFinalPage() {
     if (document.querySelectorAll(".selected").length == 3) {
         let itemsFinalPage = document.querySelectorAll(".final-page span");
         let totalPrice = document.querySelectorAll(".final-page p")[1];
+
         itemsFinalPage[0].innerText = document.querySelector(".main-course .selected h3").innerText;
         itemsFinalPage[2].innerText = document.querySelector(".drink .selected h3").innerText;
         itemsFinalPage[4].innerText = document.querySelector(".dessert .selected h3").innerText;
@@ -81,8 +87,11 @@ function showFinalPage() {
 
         totalPrice.innerText = "R$ " + totalPriceSum;
         totalPrice.innerText;
+
         finalPage.classList.toggle("display-none");
     }
+
+    return true;
 }
 
 function sendToWhatsapp() {
@@ -91,24 +100,31 @@ function sendToWhatsapp() {
     selectedMainCourse = document.querySelector(".main-course .selected h3").innerText;
     selectedDrink = document.querySelector(".drink .selected h3").innerHTML;
     selectedDessert = document.querySelector(".dessert .selected h3").innerText;
-    
 
     let message = `Olá, gostaria de fazer o pedido:\n- Prato: ${selectedMainCourse}\n- Bebida: ${selectedDrink}\n- Sobremesa: ${selectedDessert}\nTotal: R$ ${totalPriceSum}\n\nNome: ${name}\nEndereço: ${address}`;
 
     window.open(`https://wa.me/5511994107417?text=${encodeURIComponent(message)}`, '_blank');
 
     orderSentToWhatsapp();
+
+    return true;
 }
 
 function orderSentToWhatsapp() {
     document.querySelector(".final-page > div").innerHTML = "<h4 style='text-align: center;'>Finalize seu pedido pelo Whatsapp</h4><button class='confirm-order' onclick='resetFinalPage();'>Fechar</button>";
+
+    return true;
 }
 
 function resetFinalPage() {
     document.querySelector(".final-page > div").innerHTML = finalPageCopy.innerHTML;
     hideFinalPage();
+
+    return true;
 }
 
 function hideFinalPage() {
     finalPage.classList.toggle("display-none");
+
+    return true;
 }
