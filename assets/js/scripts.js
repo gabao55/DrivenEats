b07a2d68b98777a4410c80441865bc642c47fe53
gabao55 <gabriel.s.rosin@gmail.com>
2022-06-10 08:33:35 -0300

    Implements features for sending the order to whatsapp and the screen after sending to whatsapp

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,8 +1,7 @@
 let finishButton = document.querySelector(".footer button");
 let notFinishedButton = document.querySelector(".footer button p").cloneNode(true);
 let finalPage = document.querySelector(".final-page");
-let itemsFinalPage = document.querySelectorAll(".final-page span");
-let totalPrice = document.querySelectorAll(".final-page p")[1];
+let finalPageCopy = document.querySelector(".final-page > div").cloneNode(true);
 let totalPriceSum;
 let selectedMainCourse;
 let selectedDrink;
@@ -65,6 +64,8 @@ document.addEventListener("click", () => {
 
 function showFinalPage() {
     if (document.querySelectorAll(".selected").length == 3) {
+        let itemsFinalPage = document.querySelectorAll(".final-page span");
+        let totalPrice = document.querySelectorAll(".final-page p")[1];
         itemsFinalPage[0].innerText = document.querySelector(".main-course .selected h3").innerText;
         itemsFinalPage[2].innerText = document.querySelector(".drink .selected h3").innerText;
         itemsFinalPage[4].innerText = document.querySelector(".dessert .selected h3").innerText;
@@ -90,17 +91,23 @@ function sendToWhatsapp() {
     selectedDessert = document.querySelector(".dessert .selected h3").innerText;
     
 
-    let message = `Olá, gostaria de fazer o pedido:\n
-    - Prato: ${selectedMainCourse}\n
-    - Bebida: ${selectedDrink}\n
-    - Sobremesa: ${selectedDessert}\n
-    Total: R$ ${totalPriceSum}\n
-    \n
-    Nome: ${name}\n
-    Endereço: ${address}`;
+    let message = `Olá, gostaria de fazer o pedido:\n- Prato: ${selectedMainCourse}\n- Bebida: ${selectedDrink}\n- Sobremesa: ${selectedDessert}\nTotal: R$ ${totalPriceSum}\n\nNome: ${name}\nEndereço: ${address}`;
 
-    console.log(message);
+    window.open(`https://wa.me/5511994107417?text=${encodeURIComponent(message)}`, '_blank');
 
+    orderSentToWhatsapp();
+
+    // TODO: Implement the functionalities to send message to Whatsapp
+
+}
+
+function orderSentToWhatsapp() {
+    document.querySelector(".final-page > div").innerHTML = "<h4 style='text-align: center;'>Finalize seu pedido pelo Whatsapp</h4><button class='confirm-order' onclick='resetFinalPage();'>Fechar</button>";
+}
+
+function resetFinalPage() {
+    document.querySelector(".final-page > div").innerHTML = finalPageCopy.innerHTML;
+    hideFinalPage();
 }
 
 function hideFinalPage() {
